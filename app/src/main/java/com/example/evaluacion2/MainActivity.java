35861ec14780aac9fe7aa4e20908033435bb46e7
Juan Clemente <j.clemente3@alumnos.santotomas.cl>
2023-10-16 15:59:16 -0300

    desarrollado hasta las interfaces

--- app/src/main/java/com/example/evaluacion2/MainActivity.java
+++ app/src/main/java/com/example/evaluacion2/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.evaluacion2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    Handler handler;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(
+                        getApplicationContext(), ingreso.class
+                );
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
